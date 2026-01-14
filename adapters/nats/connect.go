f0a7b3a4d74cd51ca5e4498beae0b7ad9b85a63b
Timo Friedl <timofriedlberlin@gmail.com>
2026-01-14 10:36:03 +0100

    feat(nats): add reusable connection wrapper with mutex for safe shared access

--- adapters/nats/connect.go
+++ adapters/nats/connect.go
@@ -2,11 +2,27 @@ package nats
 
 import (
 	"os"
+	"sync"
 
 	natsgo "github.com/nats-io/nats.go"
 )
 
 type Connector func() (*natsgo.Conn, error)
+
+func ReuseConnection(connect Connector) Connector {
+	var mu sync.Mutex
+	var nc *natsgo.Conn
+	return func() (*natsgo.Conn, error) {
+		mu.Lock()
+		defer mu.Unlock()
+		if nc == nil {
+			var err error
+			nc, err = connect()
+			return nc, err
+		}
+		return nc, nil
+	}
+}
 
 func ConnectURL(natsURL string) Connector {
 	return func() (*natsgo.Conn, error) {
